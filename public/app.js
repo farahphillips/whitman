2298d2bdae74f7d3026b408373081f814a18ce3a
fmaredia <farhana.maredia@gmail.com>
2015-07-13 19:54:52 -0500

    fifth.

--- public/app.js
+++ public/app.js
@@ -2,37 +2,43 @@ var app = angular.module('app', [])
 
 app.controller('poemController', function($scope, $http) {
   $scope.load = function() {
-  // set headers
-  $http.defaults.headers.common['X-Mashape-Key'] = "EtTWWeqTGPmshQS4DrsWSju5uSdTp1GVq82jsnjZdH1sIWBdxM"
-  
-  // request poem-list array from API
-  $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/').
-    success(function(data, status, headers, config) {
-      // poem list array
-      var list = data
+    // set headers
+    $http.defaults.headers.common['X-Mashape-Key'] = "EtTWWeqTGPmshQS4DrsWSju5uSdTp1GVq82jsnjZdH1sIWBdxM"
+    
+    // request poem-list array from API
+    $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/').
+      success(function(data, status, headers, config) {
+        // poem list array
+        $scope.list = data
 
-      // filtering out broken 'book' poems from the poem list array
-      var listFilter = function(str) { return str.indexOf("book") == -1 }
-      list = _.filter(list, listFilter)
-
-      // get a random poem id from the array
-      var index = Math.ceil(Math.random() * list.length)
-      var id = list[index]
-
-      // inner-request for the above poem id
-      $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/' + id).
-        success(function(data, status, headers, config) {
-          $scope.title = data.title
-          $scope.poem = data.text
-        }). // error with inner-request for a specific poem
-        error(function(data, status, headers, config) {
-          console.log("error")
-        });
-    }). // error with poem-list request
-    error(function(data, status, headers, config) {
-      console.log("error")
-    });
+        // filtering out broken 'book' poems from the poem list array
+        var listFilter = function(str) { return str.indexOf("book") == -1 }
+        $scope.list = _.filter($scope.list, listFilter)
+        
+        // get initial poem
+        $scope.getPoem()
+      }). // error with poem-list request
+      error(function(data, status, headers, config) {
+        console.log("error")
+      });
   }
 
+  $scope.getPoem = function() {
+    // get a random poem id from the array
+    var index = Math.floor(Math.random() * $scope.list.length)
+    var id = $scope.list[index]
+
+    // inner-request for the above poem id
+    $http.get('https://pafmon-walt-whitman-poems.p.mashape.com/poems/' + id).
+      success(function(data, status, headers, config) {
+        $scope.title = data.title
+        $scope.poem = data.text
+      }). // error with inner-request for a specific poem
+      error(function(data, status, headers, config) {
+        console.log("error")
+      });
+  }
+
+  // initial load
   $scope.load()
 })
